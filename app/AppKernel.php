600b6b8636479b658ab7949eec9b192b164933d2
Daniel Rotter <daniel.rotter@massiveart.com>
2013-08-23 09:51:04 +0200

    added SuluContactBundle as dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Sulu\Bundle\AdminBundle\SuluAdminBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Sulu\Bundle\ContactBundle\SuluContactBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
