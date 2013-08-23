0525774ce640714ea1632f5e7a504cd12edaf5dc
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-23 08:00:40 +0200

    added SuluGeneratorBundle and phpunit to require-dev

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Sulu\Bundle\GeneratorBundle\SuluGeneratorBundle();
         }
 
         return $bundles;
