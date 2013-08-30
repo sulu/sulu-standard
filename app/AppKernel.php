58822b0998075b863941add7ea56bd803bf08c9d
Daniel Rotter <daniel.rotter@massiveart.com>
2013-08-30 09:45:22 +0200

    added doctrine extensions bundle to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Sulu\Bundle\ContactBundle\SuluContactBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
