a4ca03dff372b9549e52d6ceec9c14064c3f5d90
Thomas Schedler <thomas@chirimoya.at>
2013-10-27 16:04:31 +0100

    upated travis & saucelabs config

--- tests/WebTest/Sulu/Bundle/AdminBundleTest.php
+++ tests/WebTest/Sulu/Bundle/AdminBundleTest.php
@@ -11,6 +11,7 @@
 namespace WebTest\Sulu\Bundle;
 
 use Sauce\Sausage\WebDriverTestCase;
+use Sauce\Sausage\SauceTestCommon;
 
 class AdminBundleTest extends WebDriverTestCase
 {
@@ -18,7 +19,7 @@ class AdminBundleTest extends WebDriverTestCase
     public static $browsers = array(
         array(
             'browserName' => 'firefox',
-            'host' => 'localhost',
+            'host' => '%s:%s@localhost',
             'port' => 4445,
             'desiredCapabilities' => array(
                 'platform' => 'Windows 2012',
@@ -26,10 +27,10 @@ class AdminBundleTest extends WebDriverTestCase
         ),
         array(
             'browserName' => 'chrome',
-            'host' => 'localhost',
+            'host' => '%s:%s@localhost',
             'port' => 4445,
             'desiredCapabilities' => array(
-                'platform' => 'Windows 2012'
+                'platform' => 'Windows 2012',
             )
         ),
         /*array(
@@ -37,6 +38,43 @@ class AdminBundleTest extends WebDriverTestCase
             'local' => true,
         ),*/
     );
+
+    // TODO make abstract TestCase class
+    public function setupSpecificBrowser($params)
+    {
+
+        if (getenv('SAUCE_USERNAME') && getenv('SAUCE_ACCESS_KEY')) {
+            define(SAUCE_USERNAME, getenv('SAUCE_USERNAME'));
+            define(SAUCE_ACCESS_KEY, getenv('SAUCE_ACCESS_KEY'));
+        }
+
+        SauceTestCommon::RequireSauceConfig();
+
+        if (isset($params['host'])) {
+            $params['host'] = sprintf($params['host'], SAUCE_USERNAME, SAUCE_ACCESS_KEY);
+        }
+
+
+        if (getenv('TRAVIS_JOB_NUMBER')) {
+
+            $capabilities = array();
+
+            $capabilities['tunnel-identifier'] = getenv('TRAVIS_JOB_NUMBER');
+            $capabilities['build'] = getenv('TRAVIS_BUILD_NUMBER');
+            $capabilities['tags'] = array('Travis-CI', 'PHP ' . phpversion());
+
+            if (isset($params['desiredCapabilities'])) {
+                $params['desiredCapabilities'] = array_merge(
+                    $params['desiredCapabilities'],
+                    $capabilities
+                );
+            } else {
+                $params['desiredCapabilities'] = $capabilities;
+            }
+        }
+
+        parent::setupSpecificBrowser($params);
+    }
 
     public function setUpPage()
     {
