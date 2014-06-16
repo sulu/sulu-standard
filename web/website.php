caad2c4d67e625424b69f11b12f2e177301faf77
Thomas Schedler <thomas@chirimoya.at>
2014-06-16 16:20:09 +0200

    disabled cashing for dev mode

--- web/website.php
+++ web/website.php
@@ -23,11 +23,15 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__ . '/../app/website/AppKernel.php';
-require_once __DIR__ . '/../app/website/AppCache.php';
 
 $kernel = new AppKernel(APP_ENV, (APP_ENV == 'dev') ? true : false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+
+if (APP_ENV != 'dev') {
+    require_once __DIR__ . '/../app/website/AppCache.php';
+    $kernel = new AppCache($kernel);
+}
+
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
