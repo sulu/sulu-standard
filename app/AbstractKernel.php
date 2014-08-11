864d19ad5423a1b642e488268cb72c9b34ae3544
Thomas Schedler <thomas@chirimoya.at>
2014-08-11 17:09:14 +0200

    updated kernel bundels

--- app/AbstractKernel.php
+++ app/AbstractKernel.php
@@ -12,6 +12,7 @@ abstract class AbstractKernel extends Kernel
     private $context = null;
 
     const CONTEXT_ADMIN = 'admin';
+
     const CONTEXT_WEBSITE = 'website';
 
     /**
@@ -20,24 +21,29 @@ abstract class AbstractKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            // symfony standard
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            // symfony cmf
             new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
 
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            // doctrine extensions
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
-
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Liip\ThemeBundle\LiipThemeBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
+            // rest
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+
+            // sulu
             new Sulu\Bundle\ContactBundle\SuluContactBundle(),
             new Sulu\Bundle\MediaBundle\SuluMediaBundle(),
             new Sulu\Bundle\SecurityBundle\SuluSecurityBundle(),
@@ -49,17 +55,21 @@ abstract class AbstractKernel extends Kernel
             new Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle(),
             new Sulu\Bundle\LocationBundle\SuluLocationBundle(),
 
+            // website
             new Client\Bundle\WebsiteBundle\ClientWebsiteBundle(),
-            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+            new Liip\ThemeBundle\LiipThemeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            // symfony standard
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            // sulu
             $bundles[] = new Sulu\Bundle\GeneratorBundle\SuluGeneratorBundle();
 
-            // Debug
+            // debug enhancement
             $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
         }
 
@@ -79,7 +89,7 @@ abstract class AbstractKernel extends Kernel
      */
     public function getCacheDir()
     {
-        return $this->rootDir.'/cache/' . $this->getContext() . '/'.$this->environment;
+        return $this->rootDir . '/cache/' . $this->getContext() . '/' . $this->environment;
     }
 
     /**
@@ -87,7 +97,7 @@ abstract class AbstractKernel extends Kernel
      */
     public function getLogDir()
     {
-        return $this->rootDir.'/logs/' . $this->getContext() . '/'.$this->environment;
+        return $this->rootDir . '/logs/' . $this->getContext() . '/' . $this->environment;
     }
 
     /**
@@ -99,9 +109,12 @@ abstract class AbstractKernel extends Kernel
     protected function getContext()
     {
         if (null === $this->context) {
-            throw new \RuntimeException(sprintf(
-                'No context has been set for kernel "%s"', get_class($this)
-            ));
+            throw new \RuntimeException(
+                sprintf(
+                    'No context has been set for kernel "%s"',
+                    get_class($this)
+                )
+            );
         }
 
         return $this->context;
