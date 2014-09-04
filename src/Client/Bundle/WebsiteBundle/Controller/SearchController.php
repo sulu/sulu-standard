151c104bb972ddb5e315c85b638d38c2ae01da8d
Johannes Wachter <johannes.wachter@massiveart.com>
2014-09-04 15:38:08 +0200

    added localized search manager in search controller

--- src/Client/Bundle/WebsiteBundle/Controller/SearchController.php
+++ src/Client/Bundle/WebsiteBundle/Controller/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace Client\Bundle\WebsiteBundle\Controller;
 
+use Sulu\Bundle\SearchBundle\LocalizedSearchManager\LocalizedSearchManagerInterface;
+use Sulu\Component\Webspace\Analyzer\RequestAnalyzerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,13 +12,24 @@ class SearchController extends Controller
     public function queryAction(Request $request)
     {
         $query = $request->query->get('q', '');
+        $locale = $request->query->get('l', '');
 
-        $searchManager = $this->get('massive_search.search_manager');
-        $hits = $searchManager->search($query, 'content');
+        /** @var LocalizedSearchManagerInterface $searchManager */
+        $searchManager = $this->get('sulu_search.localized_search_manager');
 
-        return $this->render('ClientWebsiteBundle:views:query.html.twig', array(
-            'query' => $query,
-            'hits' => $hits,
-        ));
+        /** @var RequestAnalyzerInterface $requestAnalyzer */
+        $requestAnalyzer = $this->get('sulu_core.webspace.request_analyzer');
+
+        $hits = $searchManager->search($query, $locale, 'content');
+
+        return $this->render(
+            'ClientWebsiteBundle:views:query.html.twig',
+            array(
+                'query' => $query,
+                'hits' => $hits,
+                'webspaceKey' => $requestAnalyzer->getCurrentWebspace()->getKey(),
+                'locale' => $locale
+            )
+        );
     }
 }
