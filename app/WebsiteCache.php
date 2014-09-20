7bbf4cdeebbdef12825920d3acbcec4bf73d6173
Thomas Schedler <thomas@chirimoya.at>
2014-09-20 23:53:38 +0200

    added reverse-proxy ttl handling

--- app/WebsiteCache.php
+++ app/WebsiteCache.php
@@ -2,25 +2,9 @@
 
 require_once __DIR__ . '/WebsiteKernel.php';
 
-use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Sulu\Component\HttpCache\HttpCache;
 
 class WebsiteCache extends HttpCache
 {
-    protected function invalidate(Request $request, $catch = false)
-    {
-        if ('PURGE' !== $request->getMethod()) {
-            return parent::invalidate($request, $catch);
-        }
 
-        $response = new Response();
-        if ($this->getStore()->purge($request->getUri())) {
-            $response->setStatusCode(200, 'Purged');
-        } else {
-            $response->setStatusCode(200, 'Not purged'); // 404 ???
-        }
-
-        return $response;
-    }
 }
