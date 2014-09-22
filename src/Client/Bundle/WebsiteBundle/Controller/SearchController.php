761a674d11194cd9b9e8b271781c664a96ea4a28
dantleech <dan.t.leech@gmail.com>
2014-09-22 11:48:10 +0200

    Updated to latest SearchBundle

--- src/Client/Bundle/WebsiteBundle/Controller/SearchController.php
+++ src/Client/Bundle/WebsiteBundle/Controller/SearchController.php
@@ -18,13 +18,13 @@ class SearchController extends WebsiteController
         $query = $this->getRequestParameter($request, 'q', true);
 
         /** @var LocalizedSearchManagerInterface $searchManager */
-        $searchManager = $this->get('sulu_search.localized_search_manager');
+        $searchManager = $this->get('massive_search.search_manager');
 
         /** @var RequestAnalyzerInterface $requestAnalyzer */
         $requestAnalyzer = $this->get('sulu_core.webspace.request_analyzer');
         $locale = $requestAnalyzer->getCurrentLocalization()->getLocalization();
 
-        $hits = $searchManager->search($query, $locale, 'content');
+        $hits = $searchManager->createSearch($query)->locale($locale)->index('content')->go();
 
         $data = $this->getAttributes(
             array(
