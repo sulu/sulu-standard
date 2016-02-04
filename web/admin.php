43f7f6521713159bcca20ac5bbd3acb421baaee0
Daniel Rotter <daniel.rotter@gmail.com>
2016-02-04 17:09:04 +0100

    fixed styleci

--- web/admin.php
+++ web/admin.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
